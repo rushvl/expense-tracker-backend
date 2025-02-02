f6b534b08fe8e91d88fe5c2f2f66eeaf7f923e7f
Rushil Vora <rushilvora1@gmail.com>
2025-02-03 02:35:03 +0530

    fix: removed redundant return

--- middleware/authorizeRole.js
+++ middleware/authorizeRole.js
@@ -1,8 +1,6 @@
 export const authorize = (roles) => (req, res, next) => {
-    return function (req, res, next) {
-        if (!roles.includes(req.user.roleId)) {
-            return res.status(401).json({error: "Invalid role, Access denied"});
-        }
-        next();
+    if (!roles.includes(req.user.roleId)) {
+        return res.status(401).json({error: "Invalid role, Access denied"});
     }
+    next();
 }
