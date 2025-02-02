704411f3f29718a4f5000aa2033b7fe3d2ae1e66
Rushil Vora <rushilvora1@gmail.com>
2025-02-03 02:32:16 +0530

    feat: added route for a user to create expenses

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,7 +1,25 @@
 import express from 'express';
 import { authenticateToken } from '../middleware/authenticateToken.js';
 import User from '../models/user.js';
+import Expense from "../models/expense.js";
 
 const router = express.Router();
+
+router.post('/add', authenticateToken, async (req, res) => {
+    const { amount, description, category, paymentMethod, userId } = req.body;
+
+    try {
+        const expense = await Expense.create({
+            amount,
+            description,
+            category,
+            paymentMethod,
+            userId
+        })
+        res.status(201).send(expense);
+    } catch (error) {
+        res.status(400).json({message: "Failed to add expense ", error: error.message});
+    }
+})
 
 export default router;
