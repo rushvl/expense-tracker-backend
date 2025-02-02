0cf02293ca092e31c8103eb7aa21863c4054cb35
Rushil Vora <rushilvora1@gmail.com>
2025-02-03 03:26:41 +0530

    feat: added expense controllers and routes

---
+++ controllers/expenseController.js
@@ -0,0 +1,66 @@
+const Expense = require("../models/expense.js");
+
+exports.createExpense = async (req, res) => {
+    const { amount, description, category, paymentMethod, userId } = req.body;
+
+    try {
+        const expense = await Expense.create({
+            amount,
+            description,
+            category,
+            paymentMethod,
+            userId
+        })
+        res.status(201).json(expense);
+    } catch (error) {
+        res.status(400).json({message: "Failed to add expense ", error: error.message});
+    }
+}
+
+exports.getExpense = async (req, res) => {
+    const id = req.user.id;
+    try {
+        const expenses = await Expense.findAll({where: {userId: id}});
+        res.status(201).json(expenses);
+    } catch(error) {
+        res.status(500).json({success:false, message: 'Unable to list expenses', error: error.message});
+    }
+}
+
+exports.updateExpense = async (req, res) => {
+    const { amount, description, category, paymentMethod, date, userId } = req.body;
+
+    try {
+        const expense = await Expense.findOne({ where: { id:req.params.id }});
+        if (!expense) {
+            res.status(404).json({error: 'Expense not found'});
+        }
+
+        expense.amount = amount !== undefined ? amount : expense.amount;
+        expense.description = description !== undefined ? description : expense.description;
+        expense.category = category !== undefined ? category : expense.category;
+        expense.paymentMethod = paymentMethod !== undefined ? paymentMethod : expense.paymentMethod;
+        expense.date = date !== undefined ? date : expense.date;
+
+        await expense.save();
+
+        res.status(201).json({message: 'Expense updated successfully'});
+    } catch (e) {
+        res.status(500).json({message: 'Unable to update expense ', error: e});
+    }
+}
+
+exports.deleteExpense = async (req, res) => {
+    try {
+        const expense = await Expense.findOne({ where: { id:req.params.id }});
+
+        if (!expense) {
+            res.status(404).json({error: 'Expense not found'});
+        }
+
+        await expense.destroy();
+        res.status(201).json({ success: true, message: 'Expense deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'Failed to delete expense', error: err.message });
+    }
+}
--- routes/expense-routes.js
+++ routes/expense-routes.js
@@ -1,5 +1,13 @@
 import express from 'express';
+import {authenticateToken} from "../middleware/authenticateToken.js";
+import Expense from "../models/expense.js";
+import {createExpense, deleteExpense, getExpense, updateExpense} from "../controllers/expenseController.js";
 
 const router = express.Router();
+
+router.post('/expenses', authenticateToken, createExpense);
+router.get('/expenses', authenticateToken, getExpense);
+router.put('/expenses/:id', authenticateToken, updateExpense);
+router.delete('/expenses/:id', authenticateToken, deleteExpense);
 
 export default router;
