afb871c3461dce7fa79d2dfaabf6391ae587d4e1
Rushil Vora <rushilvora1@gmail.com>
2025-02-03 03:26:21 +0530

    chore: moved /add route to expense-routes.js

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -4,21 +4,4 @@ import Expense from "../models/expense.js";
 
 const router = express.Router();
 
-router.post('/add', authenticateToken, async (req, res) => {
-    const { amount, description, category, paymentMethod, userId } = req.body;
-
-    try {
-        const expense = await Expense.create({
-            amount,
-            description,
-            category,
-            paymentMethod,
-            userId
-        })
-        res.status(201).send(expense);
-    } catch (error) {
-        res.status(400).json({message: "Failed to add expense ", error: error.message});
-    }
-})
-
 export default router;
