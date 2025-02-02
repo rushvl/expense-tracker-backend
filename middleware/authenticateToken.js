184b2a3cc860b50625e322e67b30998e26b1084f
Rushil Vora <rushilvora1@gmail.com>
2025-02-03 02:38:53 +0530

    chore: removed unused import, redundant definitions, deleted unused file

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -1,55 +1,22 @@
 import jwt from 'jsonwebtoken';
 import User from "../models/user.js";
-import {Role} from "../models/role.js";
 
 export const authenticateToken = async (req, res, next) => {
-    const authHeader = req.headers['authorization']; //Bearer TOKEN
-    const token = authHeader && authHeader.split(' ')[1];
-
-    if (token == null) return res.status(401).json({error:"Null token"});
+    const token = req.headers.authorization?.split(' ')[1]; //Bearer Token
+    // console.log(token);
+    if (!token) return res.status(401).json({error:"Null token"});
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET, (error, user) => {
-            if (error) return res.status(403).json({error : error.message});
-            req.user = user;
-            next();
-        });
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-        const user = User.findByPk(decoded.id);
-
-        if(!user) {
-            return res.status(401).json({error:"Invalid token"});
-        }
-
+        const user = await User.findByPk(decoded.id);
+        if (!user) return res.status(401).json({ message: 'Invalid token, couldnt find user' });
+        // console.log(user);
         req.user = user;
         next();
 
     } catch(error){
         res.status(403).json({error:'Unauthorized'});
-    }
-}
-
-export const authorize = (roles) => (req, res, next) => {
-    return function(req, res, next) {
-        if(!roles.includes(req.user.roleId)){
-            return res.status(401).json({error:"Invalid role, Access denied"});
-        }
-        next();
-    }
-}
-
-export const isAdmin = async (req, res, next) => {
-    try {
-        const user = await User.findByPk(req.user.id, {
-            include: [{model: Role}],
-        });
-
-        if(user && user.roleId.role === 'admin') next();
-        else {
-            res.status(403).json({error:"Access denied"});
-        }
-    } catch (error) {
-        res.status(401).json({error: error.message});
     }
 }
 
